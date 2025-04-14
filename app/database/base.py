3d3e4a38ba9ee89fa9966e0848e8d31062c78cd8
valya_plotnikova <valya_ezhova@mail.ru>
2025-04-14 12:53:16 +0300

    created base model

---
+++ app/database/base.py
@@ -0,0 +1,12 @@
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase
+
+from core.config import settings
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
